ff6d778f2203c0532650c0c107d7c01b14b58180
Gustavo Cortes <gustavocortes934@gmail.com>
2025-08-25 09:59:07 -0300

    Se completa ejercicio 10 del tp4

--- Unidad4-EstructurasRepetitivas/ejercicio10.py
+++ Unidad4-EstructurasRepetitivas/ejercicio10.py
@@ -1,2 +1,13 @@
 # 10) Escribe un programa que invierta el orden de los dígitos de un número ingresado por el
 # usuario. Ejemplo: si el usuario ingresa 547, el programa debe mostrar 745.
+
+numero: str = input("Ingrese un número: ")
+invertido = ""
+for i in range(len(numero) - 1, -1, -1):
+    invertido += numero[i]
+print("Número invertido:", invertido)
+
+# Otra forma de hacerlo usando slicing
+# numero: str = input("Ingrese un número: ")
+# numero_invertido = numero[::-1]
+# print("Número invertido:", numero_invertido)
