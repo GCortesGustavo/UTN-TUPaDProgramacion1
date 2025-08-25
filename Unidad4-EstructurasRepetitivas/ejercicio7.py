a272a0460d7ba6a3eb0f0a3ee2566b1c6d7126e1
Gustavo Cortes <gustavocortes934@gmail.com>
2025-08-25 09:35:25 -0300

    se completa el ejercicio 7 y 8 del tp 4

--- Unidad4-EstructurasRepetitivas/ejercicio7.py
+++ Unidad4-EstructurasRepetitivas/ejercicio7.py
@@ -1,2 +1,9 @@
 # 7) Crea un programa que calcule la suma de todos los números comprendidos entre 0 y un
 # número entero positivo indicado por el usuario.
+
+num: int = int(input('Ingrese un número: '))
+suma: int = 0
+
+for i in range(0, num +1):
+    suma = suma + i
+print('La suma es: ', suma)
--- Unidad4-EstructurasRepetitivas/ejercicio8.py
+++ Unidad4-EstructurasRepetitivas/ejercicio8.py
@@ -2,3 +2,27 @@
 # programa debe indicar cuántos de estos números son pares, cuántos son impares, cuántos son
 # negativos y cuántos son positivos. (Nota: para probar el programa puedes usar una cantidad
 # menor, pero debe estar preparado para procesar 100 números con un solo cambio).
+
+numeros_negativos: int = 0
+numeros_positivos: int = 0
+numeros_pares: int = 0
+numeros_impares: int = 0
+
+num_max:int = 100
+
+for i in range(1, num_max + 1):
+    num: int = int(input('Ingresa el número: '))
+    if num % 2 == 0:
+        numeros_pares = numeros_pares + 1
+    else:
+        numeros_impares = numeros_impares + 1
+    
+    if num < 0:
+        numeros_negativos = numeros_negativos + 1
+    elif num > 0:
+        numeros_positivos = numeros_positivos + 1
+
+print('Los numeros pares son: ', numeros_pares)
+print('Los numeros impares son: ', numeros_impares)
+print('Los numeros positivos son: ', numeros_positivos)
+print('Los numeros negativos son: ', numeros_negativos)
